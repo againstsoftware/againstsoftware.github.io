3fe713b37d92930a28900321f1ff97dd36f295cb
eze <69222000+quiellll@users.noreply.github.com>
2024-12-14 23:02:19 +0100

    sticky navbar

--- js/main.js
+++ js/main.js
@@ -34,4 +34,29 @@
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
   });
+
+  // Navbar scroll spy
+  document.addEventListener("DOMContentLoaded", function () {
+    var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+      target: "#navbarNav",
+      offset: 100,
+    });
+
+    document.querySelectorAll(".navbar-nav a.nav-link").forEach((link) => {
+      if (this.hash !== "") {
+        e.preventDefault();
+        const hash = this.hash;
+
+        document.querySelector(hash).scrollIntoView({
+          behavior: "smooth",
+        });
+
+        // En móvil cerrar tras pulsar un enlace
+        const navbarToggle = document.querySelector(".navbar-toggler");
+        if (window.getComputedStyle(navbarToggle).display !== "none") {
+          document.querySelector(".navbar-collapse").classList.remove("show");
+        }
+      }
+    });
+  });
 })(jQuery);
